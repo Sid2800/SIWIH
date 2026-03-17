a088b51ea1de1517538f58d8281344dcf5bcd6a9
Sady Gonzalez SID280 <megadexter28@gmail.com>
2026-03-17 10:24:21 -0600

    fix Adicionar IP de desarrollo PC SID280

--- SIWI/settings.py
+++ SIWI/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['SIWIH','127.0.0.1','192.168.88.28','192.168.1.12']
+ALLOWED_HOSTS = ['SIWIH','127.0.0.1','192.168.88.28','192.168.88.173']
 
 # Application definition
 
